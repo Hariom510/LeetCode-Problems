342e395aeff6e2cbffb7b64f8cf0a9c65ea5c6f1
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-02-12 08:15:38 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (83.91%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int count =0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == ' ')
+                count++;
+            
+            if(count == k)
+               { s.erase(s.begin()+i, s.end());
+            break;}
+        }
+        return s;
+    }
+};
