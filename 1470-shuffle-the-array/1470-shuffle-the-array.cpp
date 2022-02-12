87a771f5a27c08ac2abdb4f72f689ade069a0bfa
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-02-12 08:43:35 +0530

    Time: 9 ms (36.12%), Space: 9.8 MB (58.87%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> arr;
+        for(int i=0; i<n; i++){
+            arr.push_back(nums[i]);
+            arr.push_back(nums[i+n]);
+                
+        }
+        return arr;
+    }
+};
