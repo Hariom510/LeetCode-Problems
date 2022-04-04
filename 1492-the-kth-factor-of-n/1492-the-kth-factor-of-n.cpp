06a152dc864c2b0e85290ca9fd21cdb120f47487
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-04-04 08:33:32 +0530

    Time: 6 ms (9.68%), Space: 6.1 MB (37.40%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> v;
+        
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                v.push_back(i);
+            }
+        }
+        if(k<=v.size()){
+            return v[k-1];
+        }
+        
+        return -1;
+    }
+};
