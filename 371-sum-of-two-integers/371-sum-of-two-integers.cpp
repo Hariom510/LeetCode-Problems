e7502e1e4aa262e35027b0399d08b88932fa7251
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-04-11 16:48:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (76.32%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        // return a+b;
+        vector<int> v{a,b};
+        return accumulate(v.begin(), v.end(), 0);
     }
 };
