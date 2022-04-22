6c46698823972828b4f2046477cb1f5a58c5a57f
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-04-22 20:30:53 +0530

    Time: 8 ms (7.12%), Space: 7.2 MB (34.10%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,10 +3,12 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         //transpose
        for(int i=0;i<matrix.size();i++)
-           {for(int j=0;j<i;j++)
+           {for(int j=0;j<=i;j++)
                {swap(matrix[i][j],matrix[j][i]);}
            }
         for(int i=0;i<matrix.size();i++){
             reverse(matrix[i].begin(),matrix[i].end());}
-    }
+    }     
+    
+    
 };
