32eb7fbfaf59f6d3a5c41f230d7c1f92202166dd
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-11 19:20:00 +0530

    Time: 130 ms (50.11%), Space: 61.8 MB (42.87%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //please  go through this solution many number of times.
+    //this is awesome question.
+    //for level order we have to use queue data structure.
+    int deepestLeavesSum(TreeNode* root) {
+        if(root== NULL) return 0;
+        queue<TreeNode*> q;
+        int sum=0;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            sum =0;
+            for(int i=0; i<sz; i++){
+                TreeNode* front = q.front();
+                q.pop();
+                sum += front->val;
+                if(front->left != NULL)q.push(front->left);
+                if(front->right != NULL) q.push(front->right);
+            }
+        }
+        return sum;
+        
+    }
+};
