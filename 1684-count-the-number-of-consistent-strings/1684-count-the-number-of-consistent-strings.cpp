974eba522c64cba490bffef0a8a1d0996320aa85
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-12 21:23:19 +0530

    Time: 141 ms (8.70%), Space: 30.3 MB (29.11%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        unordered_map<char, bool> m;
+        
+        for(auto i: allowed){
+            if(!m[i])
+            m[i] = 1;
+        }
+        int count =0;
+        for(auto x: words){
+            bool flag =1;
+            for(auto y: x){
+                if(m.find(y)== m.end()){
+                    flag =0;
+                    break;
+                }
+                    
+            }
+            if(flag ==1) count++;
+        }
+        return count;  
+    }
+};
