f0d9b0c94df13f12accd14e85e8626fbc10cad0f
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-23 17:19:46 +0530

    Time: 416 ms (18.67%), Space: 116.7 MB (78.74%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,19 +11,49 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int> v;
-        ListNode *p = head;
+//         vector<int> v;
+//         ListNode *p = head;
         
-        while(p != NULL){
-            v.push_back(p->val);
-            p = p->next;
+//         while(p != NULL){
+//             v.push_back(p->val);
+//             p = p->next;
+//         }
+//         int i=0, j= v.size()-1;
+//         int maxi = 0;
+//         while(i<j){
+//             maxi = max(maxi, v[i++]+v[j--]);
+//         }
+//         return maxi;
+        
+        
+        
+        //get middle element
+        ListNode *slow =head;
+        ListNode *fast = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;        //slow will give the middle element
+            fast = fast->next->next;
         }
-        int i=0, j= v.size()-1;
-        int maxi = 0;
-        while(i<j){
-            maxi = max(maxi, v[i++]+v[j--]);
+        
+        //reverse the second half (rembermber the below awesome logic)
+        ListNode *prev = NULL;
+        ListNode *nextnode;
+        while(slow){
+            nextnode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextnode;
         }
+        
+        int maxi =0;
+        while(prev){
+            maxi = max(maxi, head->val+prev->val);
+            prev = prev->next;
+            head = head->next;
+        }
+        
         return maxi;
-        
+           
     }
 };
