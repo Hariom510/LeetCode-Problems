9118190420bb6a08d9629c0c67f6dae2d556900b
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-29 20:00:10 +0530

    Time: 19 ms (36.57%), Space: 10.5 MB (75.59%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -10,40 +10,63 @@
  */
 class Solution {
 public:
+    // here we have to work on odd even indices not on node->val.
     ListNode* oddEvenList(ListNode* head) {
-        vector<int> v1;
-        vector<int> v2;
-        ListNode *ptr = head;
-        int count =0;
-        while(ptr!=NULL){
-            ++count;
-            if(count%2 == 0){
-                v2.push_back(ptr->val);
-            }
-            else{
-                v1.push_back(ptr->val);
-            }
-            ptr = ptr->next;
+//         vector<int> v1;
+//         vector<int> v2;
+//         ListNode *ptr = head;
+//         int count =0;
+//         while(ptr!=NULL){
+//             ++count;
+//             if(count%2 == 0){
+//                 v2.push_back(ptr->val);
+//             }
+//             else{
+//                 v1.push_back(ptr->val);
+//             }
+//             ptr = ptr->next; 
+//         }
+//         v1.insert(v1.end(), v2.begin(), v2.end());
+//         int sz = v1.size();
+//         if(sz == 0) return NULL;
+//         //steps to create a new linked list.
+//         ListNode *first = new ListNode;
+//         ListNode *temp;
+//         ListNode *last;
+        
+//         first->val = v1[0];
+//         first->next = NULL;
+//         last = first;
+        
+//         for(int i=1; i<sz; i++){
+//             temp = new ListNode;
+//             temp->val = v1[i];
+//             temp->next = NULL;
+//             last->next = temp;
+//             last = temp;
+//         }
+        
+//      return first;  
+        
+        // superb method.
+        
+        if(head == NULL) return NULL;
+        
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* odd_head = head;
+        ListNode* even_head = head->next;
+        
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
         }
-        v1.insert(v1.end(), v2.begin(), v2.end());
-        int sz = v1.size();
-        if(sz == 0) return NULL;
-        ListNode *first = new ListNode;
-        ListNode *temp;
-        ListNode *last;
+        odd->next = even_head;
         
-        first->val = v1[0];
-        first->next = NULL;
-        last = first;
+        return odd_head;
         
-        for(int i=1; i<sz; i++){
-            temp = new ListNode;
-            temp->val = v1[i];
-            temp->next = NULL;
-            last->next = temp;
-            last = temp;
-        }
         
-     return first;   
     }
 };
