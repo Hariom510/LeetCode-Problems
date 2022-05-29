afc4c874fc23d1bac7e7b48453f7d56ccfc00cfb
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-29 21:48:10 +0530

    Time: 4 ms (50.13%), Space: 7.5 MB (54.73%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,22 +11,32 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(!head || !head->next) return head;
+//         if(!head || !head->next) return head;
         
-         ListNode* dummy = new  ListNode();
-        ListNode* prev = dummy;
-        ListNode* curr= head;
-        
-        while(curr && curr->next){
-            prev->next = curr->next;
-            curr->next = prev->next->next;
-            prev->next->next = curr;
+//         ListNode* dummy = new  ListNode();
+//         ListNode* prev = dummy;
+//         ListNode* curr= head;
+//         // danger logic.
+//         while(curr && curr->next){
+//             prev->next = curr->next;
+//             curr->next = prev->next->next;
+//             prev->next->next = curr;
             
-            prev = curr;
-            curr = curr->next;
-        }
+//             prev = curr;
+//             curr = curr->next;
+//         }
         
-        return dummy->next;
+//         return dummy->next;
         
+        
+        //method 2
+        //swap the first two then recurse the rest
+        if(head == NULL || head->next == NULL) return head;
+        
+        ListNode* temp = head->next;
+        head->next = swapPairs(temp->next);
+        temp->next = head;
+        
+        return temp;
     }
 };
