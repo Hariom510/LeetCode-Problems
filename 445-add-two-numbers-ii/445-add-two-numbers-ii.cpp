cc61909172cfbf08707ce98dcfb21cd806a9d184
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-05-30 22:24:28 +0530

    Time: 70 ms (18.33%), Space: 73.7 MB (22.65%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -11,35 +11,66 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> v1, v2;
+//         vector<int> v1, v2;
         
-        while(l1!=NULL){
-            v1.push_back(l1->val);
-            l1= l1->next;
+//         while(l1!=NULL){
+//             v1.push_back(l1->val);
+//             l1= l1->next;
+//         }
+//          while(l2!=NULL){
+//             v2.push_back(l2->val);
+//             l2= l2->next;
+//         }
+//         int carry=0, sum =0;
+//         ListNode* ptr = NULL;
+//         int m= v1.size(), n= v2.size();
+//         for(int i=m-1, j= n-1; i>=0||j>=0|| carry>0; i--,j--){
+//             sum = carry;
+//             if(i>=0){
+//                 sum += v1[i];
+//             }
+//             if(j>=0){
+//                 sum += v2[j];
+//             }
+            
+//             carry = sum/10;
+//             ListNode* temp = new ListNode(sum%10);
+//             temp->next = ptr;
+//             ptr = temp;
+            
+//         }
+//         return ptr;
+        
+        //By using stack
+        
+        stack<int> s1,s2;
+        
+        while(l1){
+            s1.push(l1->val);
+            l1 = l1->next;
         }
-         while(l2!=NULL){
-            v2.push_back(l2->val);
-            l2= l2->next;
+        while(l2){
+            s2.push(l2->val);
+            l2 = l2->next;
         }
-        int carry=0, sum =0;
-        ListNode* ptr = NULL;
-        int m= v1.size(), n= v2.size();
-        for(int i=m-1, j= n-1; i>=0||j>=0|| carry>0; i--,j--){
+        int carry =0;
+        int sum;
+        ListNode *curr = NULL;
+        while(!s1.empty() || !s2.empty() || carry){
             sum = carry;
-            if(i>=0){
-                sum += v1[i];
+            if(!s1.empty()){
+                sum += s1.top();
+                s1.pop();
             }
-            if(j>=0){
-                sum += v2[j];
+            if(!s2.empty()){
+                sum += s2.top();
+                s2.pop();
             }
-            
             carry = sum/10;
-            ListNode* temp = new ListNode(sum%10);
-            temp->next = ptr;
-            ptr = temp;
-            
+            ListNode *temp = new ListNode(sum%10);
+            temp->next = curr;
+            curr = temp;
         }
-        return ptr;
-        
+        return curr;
     }
 };
