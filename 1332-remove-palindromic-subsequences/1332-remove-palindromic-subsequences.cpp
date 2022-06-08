5b6b3272cc79b3c0c350adb01b6987014acb0b71
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-06-08 06:38:31 +0530

    Time: 5 ms (17.09%), Space: 6.2 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -3,7 +3,6 @@ public:
     int removePalindromeSub(string s) {
         //please remember it that maximum only two steps are required.
         int ans =0;
-        
         if(s =="") return 0;
         
         for(int i=0, j=s.size()-1; i<j; i++,j--){
