cc27f2cf1facac598c4fc91219fa1aadd05eacdd
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-08-14 17:05:16 +0530

    Time: 60 ms (14.70%), Space: 26.8 MB (99.98%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //you need to rem the logic
+    bool checkPossibility(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            if(nums[i]>nums[i+1]){
+                if(i-1>=0 && nums[i-1]>nums[i+1]){
+                    nums[i+1]= nums[i];
+                }
+                else{
+                    nums[i]= nums[i+1];
+                }
+                break;
+            }
+        }
+        for(int i=0; i<n-1; i++){
+            if(nums[i] > nums[i+1])
+                return false;
+        }
+        return true;
+        
+    }
+};
