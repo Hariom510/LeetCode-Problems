362c5f9f8986fae33c96522e89a55058ccfc08ea
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-11-15 15:38:25 +0530

    Time: 76 ms (20.48%), Space: 31 MB (20.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,19 +12,30 @@
 class Solution {
 public:
    
-    void pre(TreeNode* root, vector<int> &v){
-        if(root==NULL)return;
+//     void pre(TreeNode* root, vector<int> &v){
+//         if(root==NULL)return;
         
-        v.push_back(root->val);
-        pre(root->left,v);
-        pre(root->right,v);
-        return;
-    }
+//         v.push_back(root->val);
+//         pre(root->left,v);
+//         pre(root->right,v);
+//         return;
+//     }
     int countNodes(TreeNode* root) {
         // int res =0;
-        vector<int> v;
-        pre(root,v);
+        // vector<int> v;
+        // pre(root,v);
+        // //in this method the time complexity will be o(n)
+        // return v.size();
         
-        return v.size();
+        if(root==NULL)return 0;
+        int hl=0,hr=0;
+        TreeNode* l=root;
+        TreeNode* r=root; 
+        
+        while(l){hl++; l=l->left;}
+        while(r){hr++; r=r->right;}
+        if(hl==hr)return pow(2,hl)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
+          
     }
 };
