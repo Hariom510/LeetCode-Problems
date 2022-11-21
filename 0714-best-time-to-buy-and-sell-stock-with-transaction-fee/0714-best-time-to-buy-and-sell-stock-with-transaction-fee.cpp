62fe6425109de4d003e2d7df9ab41bef0ed5c7b5
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2022-11-21 14:41:11 +0530

    Time: 535 ms (23.42%), Space: 96.4 MB (6.77%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -5,11 +5,11 @@ public:
         if(dp[i][buy]!=-1)return dp[i][buy];
         int profit =0;
         if(buy==1){
-            profit = max(-prices[i]+solve(prices,fee,i+1,0,n,dp),
+            profit = max(-prices[i]-fee+solve(prices,fee,i+1,0,n,dp),
                         0+solve(prices,fee,i+1,1,n,dp));
         }
         else{
-            profit = max(prices[i]-fee+solve(prices,fee,i+1,1,n,dp),
+            profit = max(prices[i]+solve(prices,fee,i+1,1,n,dp),
                         0+solve(prices,fee,i+1,0,n,dp));
         }
         return dp[i][buy]=profit;
