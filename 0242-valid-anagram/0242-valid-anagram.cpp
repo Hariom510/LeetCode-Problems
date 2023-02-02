5760ad7c21ccd25b2937b9e3c94cb903e14b9006
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-02-02 22:28:19 +0530

    Time: 6 ms (87.81%), Space: 7.3 MB (84.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> m;
+        for(int i=0; i<t.size(); i++){
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for(auto val: m){
+            if(val.second) return false;
+        }
+        return true;
+    }
+    
+    //mm
+};
