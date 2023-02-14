53d899f8b1c61f4c243c1eb4b1e63ba5808786cb
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-02-14 10:11:21 +0530

    Time: 4 ms (66.05%), Space: 12.7 MB (9.79%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
-        vector<int> left(nums.size());
-        vector<int> right(nums.size());
-        int sum =0;
-        for(int i=0; i<nums.size(); i++){
-            left[i] = sum;
-            sum += nums[i];
+        int n = nums.size();
+        vector<int> pre(n);
+        int total=0;
+        for(int i=0; i<n; i++){
+            total+= nums[i];
+            pre[i]= total;
         }
-        sum =0;
-        for(int i=nums.size()-1; i>=0; i--){
-            right[i] = sum;
-            sum += nums[i];
+        for(int i=0; i<n; i++){
+            if(total-pre[i]+nums[i]==pre[i])return i;
         }
+        return -1;      
         
-        for(int i=0; i<nums.size(); i++){
-                if(left[i] == right[i])
-                return i;
-            }
+        
+        
+        
+//         vector<int> left(nums.size());
+//         vector<int> right(nums.size());
+//         int sum =0;
+//         for(int i=0; i<nums.size(); i++){
+//             left[i] = sum;
+//             sum += nums[i];
+//         }
+//         sum =0;
+//         for(int i=nums.size()-1; i>=0; i--){
+//             right[i] = sum;
+//             sum += nums[i];
+//         }
+        
+//         for(int i=0; i<nums.size(); i++){
+//                 if(left[i] == right[i])
+//                 return i;
+//             }
             
-        return -1;
+//         return -1;
     }
 };
