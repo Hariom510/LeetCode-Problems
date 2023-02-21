5fa61684a6f49a2b38a6779dd6883e0b401cfb6b
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-02-21 11:14:50 +0530

    Time: 23 ms (91.47%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        map<int,int> m;
-        int n=nums.size();
-        for(auto i:nums){
-            m[i]++;
-        }
-        for(int i=0; i<n; i++){
-            if(m[nums[i]]==1)return nums[i];
-        }
+        // map<int,int> m;
+        // int n=nums.size();
+        // for(auto i:nums){
+        //     m[i]++;
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(m[nums[i]]==1)return nums[i];
+        // }
+        // return 0;
         
-        return 0;
+        //Binary search
+        //at even index first element is there and at 
+        // odd indx its duplicate is there
+        int left =0, right=nums.size()-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if((mid%2==0 && nums[mid]==nums[mid+1]) ||
+               (mid%2!=0 && nums[mid]==nums[mid-1])){
+               //that means there is no problem till mid
+                left= mid+1;
+            }
+            else{
+                //thre is a problem between left to middle
+                right = mid;
+            }
+        }
+        return nums[left];
         
     }
 };
