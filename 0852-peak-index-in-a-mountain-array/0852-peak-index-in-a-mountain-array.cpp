4c65ce152d3ff87ffa177b66749a8c25a50794d9
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-03-13 20:35:03 +0530

    Time: 133 ms (21.94%), Space: 59.7 MB (73.19%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -15,14 +15,18 @@ public:
 //         return idx;
         int n = arr.size();
         int l=0,r=n-1;
-        while(l<=r){
+        while(l<r){
            int mid = (l+r)/2;
-            if(arr[mid]>arr[mid+1]){
-                r = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
+            // if(arr[mid]>arr[mid+1]){
+            //     r = mid-1;
+            // }
+            // else{
+            //     l = mid+1;
+            // }
+            if (arr[mid] < arr[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
         }
         return l;
         
