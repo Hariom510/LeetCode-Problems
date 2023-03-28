d057080e6ec6a5c80017fe6cf9f1a114eac56b64
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-03-28 08:00:10 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    //memoization approach
     int solve(vector<int>& days, vector<int>& costs, int idx, int n,vector<int>& dp){
         if(idx>=n)return 0;
         if(dp[idx]!=-1)return dp[idx];
         int option1 = costs[0]+ solve(days, costs,idx+1,n,dp);
         
         int i;
+        //for 7 days pass
         for(i=idx; i<n && days[i]<days[idx]+7; i++);
         
         int option2 = costs[1]+ solve(days, costs, i,n,dp);
         
+        //for 30 days pass
         for(i=idx; i<n && days[i]<days[idx]+30;i++);
         
         int option3 = costs[2]+ solve(days,costs, i,n,dp);
@@ -20,6 +23,7 @@ public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int> dp(n+1, -1);            
-        return solve(days, costs, 0, n,dp); 
+        return solve(days, costs, 0, n,dp);
+        
     }
 };
