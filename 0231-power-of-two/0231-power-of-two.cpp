16275fa0542391e1cdf9f901d00e700e7d33ca2c
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-04-24 17:41:59 +0530

    Time: 3 ms (53.10%), Space: 6 MB (14.59%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+//         if(n==0){
+//             return false;
+//         }
+//             while(n%2 ==0){
+//                 n = n/2;
+//             }
+        
+//         if(n==1)
+//             return true;
+//         else
+//             return false;
+        
+        if(n>1){
+            while(n%2==0){
+                 n /= 2;
+            }
+        }
+        
+        return n==1;
+        
+        
+    }
+};
