cb968c56e3a6616f172f141a46f259a6869c9059
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-04-24 17:36:44 +0530

    Time: 3 ms (32.18%), Space: 7.7 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -7,15 +7,15 @@ public:
             pq.push(i);
         }
         
-        while(n>1){
+        while(pq.size()>1){
             int x= pq.top();
             pq.pop();
             int y = pq.top();
             pq.pop();
-            n -= 2;
+            // n -= 2;
             if(x-y>0){
                 pq.push(x-y);
-                n +=1;
+                // n +=1;
             }
         }
         if(pq.size()!=0)return pq.top();
