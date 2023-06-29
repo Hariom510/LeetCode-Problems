8366eaea74917f850eba69071b2e95be012ae47a
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-06-29 08:05:59 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (58.56%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        // gcd of two number is obtained by dividing the larger number by a smaller number and get the last divisior.
+        //Very unique solution
         int n1 = str1.size();
         int n2 = str2.size();
-        return (str1+str2 == str2+str1) ? str1.substr(0, gcd(n1,n2)): "";
+        
+        return (str1+str2 == str2+str1 ? str1.substr(0, __gcd(n1,n2)) : "" );
     }
-    
 };
