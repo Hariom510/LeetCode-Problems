1f2ad3b5886e51f825c877644cbe9d8579864e57
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-07-01 21:35:20 +0530

    Time: 24 ms (76.74%), Space: 19.2 MB (74.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,13 +3,22 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n= nums.size();
         // int i=0, j= n-1;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]==0 && nums[j]!=0){
-                    swap(nums[i], nums[j]);
-                    break;
-                }
-            }
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i]==0 && nums[j]!=0){
+        //             swap(nums[i], nums[j]);
+        //             break;
+        //         }
+        //     }
+        // }
+        
+         int a =0;
+         for(int i=0; i<n; i++){
+           if(nums[i]!=0){
+              swap(nums[a], nums[i]);
+               a++;
+           }
+             
         }
     }
 };
