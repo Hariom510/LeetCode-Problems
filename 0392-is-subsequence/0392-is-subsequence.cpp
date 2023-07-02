6b7354b9dcf2fc55daed02d13f5413d3d9c808fb
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-07-02 07:29:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.36%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
-    bool solve(string s, string t, int n1,int n2){
-        if(n1==0)return true;
-        if(n2==0)return false;
-        //we are traversing from last character
-        //if last char of two strings matching
-        if(s[n1-1]==t[n2-1]){
-           return solve(s,t,n1-1,n2-1);
-        }
-        //if last char of two strings not matching
-         return solve(s,t,n1,n2-1);
-    }
-    
     bool isSubsequence(string s, string t) {
-//         int sz = s.size();
-//         int ln = t.size();
-//         int count =0;
-//         int j=0;
-//         for(int i=0; i<sz; i++){
-//             while(j<ln){
-//                 if(s[i] == t[j]){
-//                     count++;
-//                     j++;
-//                     break;
-//                 }
-//                 j++;
-//             }
-//         }
-//         if(count == sz) return true;
-        
-//         return false;
-        
-        //2nd method
-        
-//         int j=0;
-//         for(int i=0; i<t.size() && j<s.size(); i++){
-//             if(t[i]==s[j]) j++;
-//         }
-        
-//         return j==s.size();
-        
-        //recursive approach
         int n1 = s.size();
         int n2 = t.size();
-        return solve(s,t,n1,n2);
-          
+        if(n1==0 && n2==0)return true;
+        
+        int i=0;
+        for(int j=0; j<n2; j++){
+           if(t[j]==s[i]){
+               i++;
+           } 
+           if(i==n1)return true;
+           // else cout<<s[i]<< " ";
+        }
+        
+        return false; 
     }
 };
