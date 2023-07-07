393838467c0e41b063b901d293fe9ff3071fa2ff
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-07-07 06:46:07 +0530

    Time: 22 ms (90.53%), Space: 9.9 MB (95.61%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
         int n = answerKey.size();
-        int j=0,i=0,cnt=0,ans1=0,ans2=0;
+        int ans1=0, ans2=0;
+        int i=0,j=0, cnt=0;
+        //for replacing F
         while(j<n){
-            if(answerKey[j]=='F'){
-                cnt++;
-            }
+            if(answerKey[j++]=='F')cnt++;
+            
             while(cnt>k){
-                if(answerKey[i]=='F')cnt--;
-                i++;
+                if(answerKey[i++]=='F')cnt--;
             }
-            j++;
-            ans1= max(ans1, j-i);
+            
+            ans1 = max(ans1, j-i);
         }
-        j=0,i=0,cnt=0;
+        
+          //for replacing T
+        i=0,j=0,cnt=0;
         while(j<n){
-            if(answerKey[j]=='T'){
-                cnt++;
-            }
+            if(answerKey[j++]=='T')cnt++;
+            
             while(cnt>k){
-                if(answerKey[i]=='T')cnt--;
-                i++;
+                if(answerKey[i++]=='T')cnt--;
             }
-            j++;
-            ans2= max(ans2, j-i);
+            
+            ans2 = max(ans2, j-i);
         }
-        return max(ans1,ans2);
+        return max(ans1, ans2);
+        
     }
 };
