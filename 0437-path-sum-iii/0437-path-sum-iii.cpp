fe7b31c5bd1839eec3d2c47b7f8dfe0e83f7a0d9
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-07-11 11:55:42 +0530

    Time: 28 ms (71.96%), Space: 15.9 MB (68.06%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,31 +11,24 @@
  */
 class Solution {
 public:
-    long long cnt=0;
-    void solve(TreeNode* root, long long targetSum){
+    long long ans =0;
+    void pre(TreeNode* root, long long targetSum){
         if(!root)return;
-        targetSum = targetSum-root->val;
-        if(targetSum==0)cnt++;
         
-        if(root->left)solve(root->left,targetSum);
-        if(root->right)solve(root->right, targetSum);
+        targetSum -= root->val;
+        if(targetSum==0)ans++;
+        
+        pre(root->left, targetSum);
+        pre(root->right, targetSum);
     }
-    
-    int pathSum(TreeNode* root, long long targetSum) {
+    int pathSum(TreeNode* root, int targetSum) {
         if(!root)return 0;
-        solve(root,targetSum);
+        if(root){
+            pre(root, targetSum);   
+        }
         if(root->left)pathSum(root->left, targetSum);
         if(root->right)pathSum(root->right, targetSum);
-        // TreeNode* head=root->left;
-        // while(head){
-        //    solve(head, targetSum); 
-        //     head = head->left;
-        // } 
-        // head=root->right;
-        // while(head){
-        //    solve(head, targetSum); 
-        //     head = head->right;
-        // } 
-        return cnt;
+        
+        return ans;  
     }
 };
