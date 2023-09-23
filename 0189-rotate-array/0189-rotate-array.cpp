e12de8ca29de348e49c43bb90bf5e85de25bda1c
Hariom Kumar <85151795+Hariom510@users.noreply.github.com>
2023-09-23 11:31:20 +0530

    Time: 19 ms (84.59%), Space: 25.2 MB (53.81%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-        //not working code
-        // int n = nums.size();
-        // reverse(nums.begin(), nums.begin()+k);
-        // reverse(nums.begin()+k, nums.end());
-        // reverse(nums.begin(), nums.end());
-        
+        //optimal solution
         int n = nums.size();
         k = k%n;
-        vector<int> v;
-        for(int i=n-1; i>=n-k; i--){
-           v.push_back(nums[i]);
-        }
-        for(int i=n-k-1; i>=0; i--){
-            nums[i+k] = nums[i];
-        }
-        reverse(v.begin(), v.end());
-        int j=0;
-        for(int i=0; i<k; i++){
-            nums[i] = v[j++];
-        }
+        reverse(nums.begin(), nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k), nums.end());
+        reverse(nums.begin(), nums.end());
+        
+        // int n = nums.size();
+        // k = k%n;
+        // vector<int> v;
+        // for(int i=n-1; i>=n-k; i--){
+        //    v.push_back(nums[i]);
+        // }
+        // for(int i=n-k-1; i>=0; i--){
+        //     nums[i+k] = nums[i];
+        // }
+        // reverse(v.begin(), v.end());
+        // int j=0;
+        // for(int i=0; i<k; i++){
+        //     nums[i] = v[j++];
+        // }
         
     }
 };
